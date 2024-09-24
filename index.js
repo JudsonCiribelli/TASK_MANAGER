38947b1fd7ffe50d144369ba4d6bcc4bb69494c8
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-09-24 11:10:37 -0300

    feat: add task model

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectToDatabase = require("./src/database/mongoose.database");
+const TaskModel = require("./src/models/task.models");
 
 //inicializando o express
 dotenv.config();
 const app = express();
 connectToDatabase();
 
-app.get("/", (req, res) => {
-  res.status(200).send("API RODANDO COM SUCESSO!");
+app.get("/", async (req, res) => {
+  const tasks = await TaskModel.find({});
+  res.status(200).send(tasks);
 });
 //iniciar servidor
 app.listen(800, () => {
