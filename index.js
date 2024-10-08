816660e80e48fbc49c149c0d239be80ed36963fb
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-10-08 13:27:01 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const taskRouter = require("./src/routes/task.routes");
+const cors = require("cors");
 
 const connectToDatabase = require("./src/database/mongoose.database");
 
 //inicializando o express
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 connectToDatabase();
 
